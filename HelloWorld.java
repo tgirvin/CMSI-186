4b02986e09840cdb62e35bc3a238e8efe2462319
tgirvin <tgirvin1@lion.lmu.edu>
2017-01-10 12:26:15 -0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+	public static void main (String [] args){
+		System.out.println("Hello, World!");
+		int i = 14;
+		System.out.println(i);
+		System.out.print("hello,");
+		System.out.println("world!");
+		for (int i = 0; i<5; i=i+1){
+			System.out.println("Hello"); // replace with i = 1, 2, 3, 4, 5 
+		}
+		for (int i = 1; i<5; i=i+3){
+			System.out.println(i); // this will equal 4, 5, 6, 7
+			// for (int i = 5; i>5; i=i-2){ this will be 5,3,1,
+	}
+}	
